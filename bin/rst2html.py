b69b1c989bb4f56693a9c61fe90e8826cecd79bb
leopoldk3 <25940620+leopoldk3@users.noreply.github.com>
2023-07-17 12:21:33 -0700

    Initial File Drop
    Addition of the first version of the application, setup with a virtual env and must have kivy installed.

---
+++ bin/rst2html.py
@@ -0,0 +1,23 @@
+#!/Users/leoklotz/Library/CloudStorage/GoogleDrive-klotzl@oregonstate.edu/My Drive/summer 2023/SE_1/Sprint 2/as5/gardenProject/bin/python
+
+# $Id: rst2html.py 9115 2022-07-28 17:06:24Z milde $
+# Author: David Goodger <goodger@python.org>
+# Copyright: This module has been placed in the public domain.
+
+"""
+A minimal front end to the Docutils Publisher, producing HTML.
+"""
+
+try:
+    import locale
+    locale.setlocale(locale.LC_ALL, '')
+except Exception:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+
+
+description = ('Generates (X)HTML documents from standalone reStructuredText '
+               'sources.  ' + default_description)
+
+publish_cmdline(writer_name='html', description=description)
---
+++ bin/rst2man.py
@@ -0,0 +1,27 @@
+#!/Users/leoklotz/Library/CloudStorage/GoogleDrive-klotzl@oregonstate.edu/My Drive/summer 2023/SE_1/Sprint 2/as5/gardenProject/bin/python
+
+# Author:
+# Contact: grubert@users.sf.net
+# Copyright: This module has been placed in the public domain.
+
+"""
+man.py
+======
+
+This module provides a simple command line interface that uses the
+man page writer to output from ReStructuredText source.
+"""
+
+import locale
+try:
+    locale.setlocale(locale.LC_ALL, '')
+except Exception:
+    pass
+
+from docutils.core import publish_cmdline, default_description
+from docutils.writers import manpage
+
+description = ("Generates plain unix manual documents.  "
+               + default_description)
+
+publish_cmdline(writer=manpage.Writer(), description=description)
